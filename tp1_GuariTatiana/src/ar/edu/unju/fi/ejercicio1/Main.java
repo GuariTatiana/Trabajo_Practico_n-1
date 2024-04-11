79d3d534d047a3afe82e5b26908c288f41f08744
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-11 17:26:28 -0300

    se agrega paquetes y clases de ejercicio1

---
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a= 8;
+		int b= 5;
+		int c= 1;
+		int d= 20;
+		int e= 9;
+		
+		int promedio= (a+b+c+d+e)/5;
+		System.out.printf("El promedio es:"+ promedio);
+	}
+
+}
