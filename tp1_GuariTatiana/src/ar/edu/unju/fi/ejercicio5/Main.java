02964ec1284e36452fb80e2c29b28d2528c0b961
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-13 14:05:56 -0300

    paquetes y clases de ejercicio5

--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -1,10 +1,9 @@
-package ar.edu.unju.fi.ejercicio4;
+package ar.edu.unju.fi.ejercicio5;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-
+		// TODO Auto-generated method stub
 
 	}
 
