3fac126563d3ff6b9c1460419de6103a4d9c6290
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-13 13:19:32 -0300

    paquetes y clases del ejercico3

--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -1,10 +1,29 @@
 package ar.edu.unju.fi.ejercicio2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//para ingresar cadena y declarar
+	
+		Scanner lector =new Scanner(System.in);
+		
+		String namp= "Argentina";
+		int edad= 18;
+		int altura= 239;
+		double precio= 1035.99; //punto decimal
+		String telefono= "388 666 789";
+		double coseno= 0.87758256189037;
+		
+		System.out.println("El nombre de un pais: " + namp);
+		System.out.println("Edad de una persona: " + edad);
+		System.out.println("La altura del edificio Alvear Tower: " + altura +"m");
+		System.out.println("El precio de la galleta oreo: " + precio);
+		System.out.println("Numero de telefono: " + telefono);
+		System.out.println("El calculo de coseno de 0.5: " + coseno);
+		
+		lector.close();	
 	}
 
 }
