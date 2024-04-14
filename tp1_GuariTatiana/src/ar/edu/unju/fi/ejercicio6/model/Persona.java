d4a8b6ee6d9e5e01726530f60607963024247e90
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-14 17:43:44 -0300

    paquetes y clases del ejercicio6

--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -1,10 +1,38 @@
 package ar.edu.unju.fi.ejercicio6.model;
 import java.time.LocalDate;
+import java.time.Period;
+
 public class Persona {
 
 	private String dni;
 	private String nombre;
 	private LocalDate fecnac;
+	private String provincia;
 	
+	public int calcularedad () {
+		LocalDate fechaActual= LocalDate.now();
+		Period edad = Period.between(fecnac, fechaActual);
+		return edad.getYears();
+	}
 	
+	public boolean mayoredad ( ) {
+		int edad= calcularedad();
+		return edad >=18;
+	}
+	
+	public void mostrarDatos () {
+		System.out.println("DNI: "+ dni);
+		System.out.println("Nombre: "+ nombre);
+		System.out.println("Fecha de nacimiento: "+ fecnac);
+		if (mayoredad()) {
+			System.out.println("La persona es MAYOR de edad");
+			
+		}
+		else {
+			System.out.println("La persona NO es MAYOR de edad");
+			
+		}
+		System.out.println("Provincia: "+ provincia);
+		
+	}
 }
