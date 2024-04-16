18571ea2bcbd7f2d2282afea621c6557b1a47cd0
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-15 21:00:28 -0300

    ejercicio 10

--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio10/main/Main.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio10/main/Main.java
@@ -1,9 +1,33 @@
 package ar.edu.unju.fi.ejercicio10.main;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio10.model.Pizza;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		 for (int i = 1; i <= 3; i++) {
+	            System.out.println("* Pizza " + i + " *");
+	            Pizza pizza = new Pizza();
+	            
+	            System.out.print("Diámetro: ");
+	            int diametro = scanner.nextInt();
+	            pizza.setDiametro(diametro);
+	            
+	            System.out.print("Ingredientes especiales (true/false): ");
+	            boolean ingredientesEspeciales = scanner.nextBoolean();
+	            pizza.setIngredientesEspeciales(ingredientesEspeciales);
+	            
+	            pizza.calcularPrecio();
+	            pizza.calcularArea();
+	            
+	            System.out.println("Precio Pizza = " + pizza.getPrecio() + "$");
+	            System.out.println("Área de la pizza = " + pizza.getArea());
+	        }
+		 scanner.close();
 
 	}
 
--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
@@ -2,4 +2,89 @@ package ar.edu.unju.fi.ejercicio10.model;
 
 public class Pizza {
 
+	private int diametro;
+    private double precio;
+    private double area;
+    private boolean ingredientesEspeciales;
+
+    private static final int ADICIONAL_20 = 500;
+    private static final int ADICIONAL_30 = 750;
+    private static final int ADICIONAL_40 = 1000;
+
+    public Pizza() {
+        // Constructor por defecto
+    }
+
+    public int getDiametro() {
+        return diametro;
+    }
+
+	public void setDiametro(int diametro) {
+		
+		 this.diametro = diametro;
+	}
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public boolean isIngredientesEspeciales() {
+        return ingredientesEspeciales;
+    }
+
+    public void setIngredientesEspeciales(boolean ingredientesEspeciales) {
+        this.ingredientesEspeciales = ingredientesEspeciales;
+    }
+
+    
+    public void calcularPrecio() { 
+	    double precioBase = 0;
+	    double precioEspecial = 0;
+
+	    switch (diametro) {
+	        case 20:
+	            precioBase = 4500;
+	            if (ingredientesEspeciales) {
+	                precioEspecial = ADICIONAL_20;
+	            }
+	            break;
+	        case 30:
+	            precioBase = 4800;
+	            if (ingredientesEspeciales) {
+	                precioEspecial = ADICIONAL_30;
+	            }
+	            break;
+	        case 40:
+	            precioBase = 5500;
+	            if (ingredientesEspeciales) {
+	                precioEspecial = ADICIONAL_40;
+	            }
+	            break;
+	        default:
+	        	System.out.print("ERROR ");
+	            break;
+	    }
+	    
+	    precio = precioBase + precioEspecial;
+	}
+	
+
+    public void calcularArea() {
+        double radio = diametro / 2.0;
+        area = Math.PI * Math.pow(radio, 2);
+        }
+
+
 }
