0fb7b5348223d0f37d954d4877d766f1043c3578
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-04-20 23:11:56 -0300

    ejercicio

--- tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio11/Main.java
+++ tp1_GuariTatiana/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -3,8 +3,14 @@ package ar.edu.unju.fi.ejercicio11;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int j=40;
+		int i=2;
+		
+		for (; j >0; j--, i++) {
+			
+			System.out.print(j*i + " ");
+		}
 	}
 
 }
